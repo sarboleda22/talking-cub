fb2bb3a1a54d9affd2112279d3352292b38e7d26
Seb <sebasarboleda22@gmail.com>
2019-11-03 20:43:05 -0500

    Add raspi files and arm32v7 docker files

--- device/constants.py
+++ device/constants.py
@@ -1,7 +1,7 @@
 # Audio recording parameters
 RATE = 44100
 CHUNK = 1600  # 100ms
-ACTIVATION_WORD = 'simba' # speaker's name
+ACTIVATION_WORD = 'hey simba' # speaker's name
 # Answer API data
 ANSWER_SVC_URL = "http://35.224.161.179/api/v1/answer/"
 ANSWER_SVC_TOKEN = "Token @hvmnt100"
--- web/run.py
+++ web/run.py
@@ -34,4 +34,4 @@ db.create_all()
 create_items()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
